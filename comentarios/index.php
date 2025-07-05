c4b80ec96f4ea564ed3ab16f7076efcf85b431ad
Vlady <19610069@utgz.edu.mx>
2025-07-05 12:56:19 -0600

    agregar form comentarios

---
+++ comentarios/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola</title>
+</head>
+<body>
+    <h1>Comentarios</h1>
+
+    <form action="POST">
+        <h2>Usuario</h2>
+        <textarea name="comentario" id="comentario"></textarea>
+    </form>
+
+
+</body>
+</html>
