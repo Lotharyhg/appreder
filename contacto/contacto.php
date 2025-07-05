5a8cc86fd69f83826076a9a45b853228d430d458
Daniel Garcia <dannysantes17@gmail.com>
2025-07-05 12:58:56 -0600

    Mostrar contactos

---
+++ contacto/contacto.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contactos</h1>
+
+    <h3>Contacto 1</h3>
+    <h3>Contacto 2</h3>
+    
+    
+</body>
+</html>
